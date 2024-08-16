0378e22b3eb0b8045fde8b431e8888db9eb2af4f
seulki <tmfrl4710@gmail.com>
2024-08-16 20:54:31 +0900

    수 찾기 이진탐색 풀이

--- src/com/olivejua/hash/BJ_1920.java
+++ src/com/olivejua/hash/BJ_1920.java
@@ -3,31 +3,55 @@ package com.olivejua.hash;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_1920 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st1.nextToken());
+        int N = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
 
-        Set<Integer> A = new HashSet<>();
         StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int[] targetNumbers = new int[N];
         for (int i = 0; i < N; i++) {
-            int number = Integer.parseInt(st2.nextToken());
-            A.add(number);
+            targetNumbers[i] = Integer.parseInt(st2.nextToken());
         }
+        Arrays.sort(targetNumbers);
 
-        StringTokenizer st3 = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st3.nextToken());
-
+        int M = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int[] searchNumbers = new int[M];
         StringTokenizer st4 = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int number = Integer.parseInt(st4.nextToken());
-            System.out.println(A.contains(number) ? 1 : 0);
+            searchNumbers[i] = Integer.parseInt(st4.nextToken());
         }
+
+        int[] result = new int[M];
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(binarySearch(targetNumbers, searchNumbers[i]) ? 1 : 0);
+        }
+    }
+
+    static boolean binarySearch(int[] targetNumbers, int searchNumber) {
+        if (targetNumbers.length == 0) {
+            return false;
+        }
+
+        int low = 0, high = targetNumbers.length-1;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+
+            if (searchNumber == targetNumbers[mid]) {
+                return true;
+            }
+
+            if (searchNumber < targetNumbers[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
     }
 }
