0e0ea23719a9bfda07a6b18f20543208bd06ba57
seulki <tmfrl4710@gmail.com>
2024-09-30 17:47:13 +0900

    삽입 정렬

--- src/com/olivejua/sort/InsertionSortList.java
+++ src/com/olivejua/sort/InsertionSortList.java
@@ -33,4 +33,26 @@ public class InsertionSortList {
 
         return parent.next;
     }
+
+    public ListNode insertionSortList2(ListNode head) {
+        ListNode parent = new ListNode();
+        ListNode p = parent;
+
+        while (head != null) {
+            while (p.next != null && p.next.val < head.val) {
+                p = p.next;
+            }
+
+            ListNode headNext = head.next;
+            ListNode pNext = p.next;
+
+            p.next = head;
+            head.next = pNext;
+
+            head = headNext;
+            p = parent;
+        }
+
+        return parent.next;
+    }
 }
--- test/com/olivejua/sort/InsertionSortListTest.java
+++ test/com/olivejua/sort/InsertionSortListTest.java
@@ -17,7 +17,7 @@ public class InsertionSortListTest {
     public void test1() {
         ListNode listNode = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
 
-        ListNode result = problem.insertionSortList(listNode);
+        ListNode result = problem.insertionSortList2(listNode);
 
         assertThat(result.val).isEqualTo(1);
         assertThat(result.next.val).isEqualTo(2);
@@ -29,7 +29,7 @@ public class InsertionSortListTest {
     public void test2() {
         ListNode listNode = new ListNode(-1, new ListNode(5, new ListNode(3, new ListNode(4, new ListNode(0)))));
 
-        ListNode result = problem.insertionSortList(listNode);
+        ListNode result = problem.insertionSortList2(listNode);
 
         assertThat(result.val).isEqualTo(-1);
         assertThat(result.next.val).isEqualTo(0);
