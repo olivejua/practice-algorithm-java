a1d108ee1a0a0b54ca6219ced2f1f464e0c3ab55
seulki <tmfrl4710@gmail.com>
2025-01-11 20:14:46 +0900

    ImplementTrie twopointer

---
+++ src/com/olivejua/trie/ImplementTrie.java
@@ -0,0 +1,84 @@
+package com.olivejua.trie;
+
+import java.util.Arrays;
+
+public class ImplementTrie implements Trie {
+    private TrieNode[] nodes;
+
+    public ImplementTrie() {
+        nodes = new TrieNode['z' - 'a' + 1];
+    }
+
+    @Override
+    public void insert(String word) {
+        if (word == null || word.isBlank()) {
+            return;
+        }
+
+        char[] letters = word.toCharArray();
+        int index = 0;
+        TrieNode[] nodes = this.nodes;
+        while (index < letters.length) {
+            if (nodes[letters[index] - 'a'] == null) {
+                nodes[letters[index] - 'a'] = new TrieNode();
+            }
+
+            if (index == letters.length-1) {
+                nodes[letters[index] - 'a'].isLast = true;
+            }
+
+            nodes = nodes[letters[index] - 'a'].children;
+            index++;
+        }
+    }
+
+    @Override
+    public boolean search(String word) {
+        char[] letters = word.toCharArray();
+        int index = 0;
+        TrieNode[] nodes = this.nodes;
+        while (index < letters.length) {
+            TrieNode node = nodes[letters[index] - 'a'];
+            if (node == null) {
+                return false;
+            }
+
+            if (index == letters.length-1) {
+                return node.isLast;
+            }
+
+            nodes = node.children;
+            index++;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean startsWith(String prefix) {
+        char[] letters = prefix.toCharArray();
+        int index = 0;
+        TrieNode[] nodes = this.nodes;
+        while (index < letters.length) {
+            TrieNode node = nodes[letters[index] - 'a'];
+            if (node == null) {
+                return false;
+            }
+
+            nodes = node.children;
+            index++;
+        }
+
+        return true;
+    }
+
+    private static class TrieNode {
+        TrieNode[] children;
+        boolean isLast;
+
+        public TrieNode() {
+            children = new TrieNode['z' - 'a' + 1];
+            isLast = false;
+        }
+    }
+}
---
+++ src/com/olivejua/trie/Trie.java
@@ -0,0 +1,7 @@
+package com.olivejua.trie;
+
+public interface Trie {
+    void insert(String word);
+    boolean search(String word);
+    boolean startsWith(String prefix);
+}
